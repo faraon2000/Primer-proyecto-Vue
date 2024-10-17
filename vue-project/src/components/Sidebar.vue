8e303ff20a33a2ce432270afe710fb1768c814ea
Rance <zurbaranvaldes@gmail.com>
2024-10-17 13:02:24 -0400

    Crea componente. Borra componentes autogenerados. Crea rutas

---
+++ vue-project/src/components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="sidebar">
+    <h2>Barra lateral</h2>
+    <ul>
+        <li><router-link to="contador">Contador</router-link></li>
+        <li><router-link to="lista-de-tareas">Lista de Tareas</router-link></li>
+    </ul>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
